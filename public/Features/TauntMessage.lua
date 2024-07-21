c12ff5f3ee9e9e7942176149ea17bd73c74146c6
Space Corp Engineering <github-ac@castle-v.com>
2024-07-21 18:08:44 +0200

    make changes in the way taunt is cast, and added new spells

--- public/Features/TauntMessage.lua
+++ public/Features/TauntMessage.lua
@@ -26,53 +26,98 @@ function ITaunted.TauntMessage.GetIcon(
     return ''
 end
 
+function ITaunted.TauntMessage.isMassTaunt(spellId)
+    local massTauntSpells = {
+        -- Warrior
+        386071, --Disrupting Shout
+        1161,   --Challenging Shout
+        46276,  --Ravager taunt
+
+        -- Paladin
+        204079, -- Final Stand
+
+        -- Monk
+        116189, --Provoke, talented
+        196727, -- Provoke #?
+
+        -- Priest
+        236003, --Visage of Terror
+
+        -- Warlock
+        -- Pets
+        43263, -- Ghoul Taunt
+
+        -- Engineering
+        82407, -- Painful Shock - caused when backfired engineering trinket goes off
+
+        -- BEES!
+        442054, -- Bees!
+
+    }
+
+    if ITaunted.Helpers.tableContainsValue(massTauntSpells, spellId) then
+        return true
+    end
+    return false
+end
+
 -- Checks if the spell is one of the taunt spells in game
 function ITaunted.TauntMessage.isTaunt(spellId)
     local tauntSpells = {
-            --Warrior
-            355, --Taunt
+        --Warrior
+        355,    --Taunt
+        145058, --Taunt #2
 
-            --Death Knight
-            51399, --Death Grip for Blood (49576 is now just the pull effect)
-            56222, --Dark Command
+        --Death Knight
+        51399,  --Death Grip for Blood (49576 is now just the pull effect)
+        56222,  --Dark Command
+        222409, --Dark Command NPC
 
-            --Paladin
-            62124, --Hand of Reckoning
+        --Paladin
+        62124, --Hand of Reckoning
 
-            --Druid
-            6795, --Growl
+        --Druid
+        6795, --Growl
 
-            --Hunter
-            20736, --Distracting Shot
+        --Hunter
+        20736,  --Distracting Shot
+        146771, --Distracting Shot #2
+        -- Pets
+        2649,   --Growl #1
+        214413, --Growl #2
+        39270,  --Growl #3
 
-            --Monk
-            115546, --Provoke
+        --Monk
+        115546, --Provoke
 
-            --Demon Hunter
-            185245, --Torment
+        --Demon Hunter
+        185245, --Torment
 
-            --Paladin
-            204079, --Final Stand
+        -- Shaman
+        204683, --Dark Whisper
+
+        -- Warlock
+        -- Pets
+        17735, --Suffering
     }
 
-     if ITaunted.Helpers.tableContainsValue(tauntSpells, spellId) then
+    if ITaunted.Helpers.tableContainsValue(tauntSpells, spellId) then
         return true
     end
     return false
 end
 
 function ITaunted.TauntMessage.playerHasValidTarget()
-    if(UnitName("target") and not UnitPlayerControlled("target")) then
+    if (UnitName("target") and not UnitPlayerControlled("target")) then
         return true
     end
     return false
 end
 
-
-
 function ITaunted.TauntMessage.handleCombatLogEvent(...)
     local sourceGUID
-    local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destRaidFlags = select(1, ...)
+    local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destRaidFlags =
+        select(1, ...)
     local spellId, spellName, spellSchool = select(12, ...)
     local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing = select(15, ...)
     local message
@@ -81,13 +126,17 @@ function ITaunted.TauntMessage.handleCombatLogEvent(...)
     local isSameTimeCasted = ITaunted.TauntMessage.lastTimeCasted == timeNow
 
 
+    if (ITaunted.TauntMessage.isMassTaunt(spellId)) then
+        message = "Mass taunting..."
+    end
+
     if (ITaunted.TauntMessage.isTaunt(spellId)) then
         if (event == "SPELL_AURA_APPLIED") then
             message = "Taunted " .. ITaunted.TauntMessage.GetIcon(destRaidFlags) .. " " .. destName
         elseif (event == "SPELL_CAST_SUCCESS") then
             message = "Taunted " .. ITaunted.TauntMessage.GetIcon(destRaidFlags) .. " " .. destName
         elseif (event == "SPELL_MISSED") then
-            message = "Taunt failed on " .. ITaunted.TauntMessage.GetIcon(destRaidFlags) .. " "  .. destName
+            message = "Taunt failed on " .. ITaunted.TauntMessage.GetIcon(destRaidFlags) .. " " .. destName
         end
 
         isSameAslastMessage = ITaunted.TauntMessage.lastMessage == message
@@ -101,11 +150,10 @@ function ITaunted.TauntMessage.handleCombatLogEvent(...)
     end
 end
 
-
 function ITaunted.TauntMessage.run()
     ITaunted.TauntMessage.Frame = CreateFrame("Frame")
     ITaunted.TauntMessage.Frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-    ITaunted.TauntMessage.Frame:SetScript("OnEvent", function (self, event, ...)
+    ITaunted.TauntMessage.Frame:SetScript("OnEvent", function(self, event, ...)
         local playerGUID = UnitGUID("player")
         local inInstance, instanceType = IsInInstance()
 
